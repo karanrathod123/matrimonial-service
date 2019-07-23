fec15be34663084fdb949c24f9b318144d22cf50
karanrathod123 <karanrathod2256@gmail.com>
2019-07-23 14:12:36 +0530

    commited

---
+++ src/main/java/com/hcl/matrimonial/service/SearchServiceImpl.java
@@ -0,0 +1,24 @@
+package com.hcl.matrimonial.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hcl.matrimonial.dto.SearchProfileDto;
+import com.hcl.matrimonial.entity.UserProfile;
+import com.hcl.matrimonial.repository.UserProfileRepository;
+
+@Service
+public class SearchServiceImpl implements SearchService {
+
+	@Autowired
+	UserProfileRepository userProfileRepository;
+
+	@Override
+	public List<UserProfile> getSearchProfile(SearchProfileDto searchProfileDto) {
+		return null;
+
+	}
+
+}

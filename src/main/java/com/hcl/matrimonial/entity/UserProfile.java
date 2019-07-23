0b39f7a05c5cc45441414fe31311b2917321efec
piyush <piyushr999@gmail.com>
2019-07-23 11:56:18 +0530

    Added User Entity

---
+++ src/main/java/com/hcl/matrimonial/entity/UserProfile.java
@@ -0,0 +1,57 @@
+package com.hcl.matrimonial.entity;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class UserProfile implements Serializable {
+
+	private static final long serialVersionUID = -3091977760078977105L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long profileId;
+
+	private String fullName;
+
+	private LocalDate dob;
+
+	private Integer age;
+
+	private String address;
+
+	private String city;
+
+	private String nationality;
+
+	private String gender;
+
+	private String mobileNumber;
+
+	private String emailId;
+
+	private Double height;
+
+	private Double weight;
+
+	private String education;
+
+	private Double annualIncome;
+
+	@JsonIgnore
+	private String userName;
+
+	@JsonIgnore
+	private String password;
+
+}

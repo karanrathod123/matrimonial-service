0429aa165c18291c8be4aa225ae9bf0c63b156fa
Harsimar <simarkaur1295@gmail.com>
2019-07-23 14:04:04 +0530

    Mertimonial Search details

--- src/main/java/com/hcl/matrimonial/repository/SearchRepository.java
+++ src/main/java/com/hcl/matrimonial/repository/SearchRepository.java
@@ -1,9 +1,12 @@
 package com.hcl.matrimonial.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.hcl.matrimonial.entity.UserProfile;
 
 public interface SearchRepository extends JpaRepository<UserProfile, Long> {
+	
+	//@Query("select * from todo t where LOWER(t.)")
 
 }

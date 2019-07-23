8663406c1f111a352afdcdb83d554978a0a19bce
amoljadhav531 <amoljadhav531@gmail.com>
2019-07-23 14:14:20 +0530

    Added return  statement

--- src/main/java/com/hcl/matrimonial/controller/LikeController.java
+++ src/main/java/com/hcl/matrimonial/controller/LikeController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LikeController {
 
-	@GetMapping
+	@GetMapping()
 	public ResponseEntity<Object> getMyLikedProfiles(long userId){
-		
+		return null;
 	}
 }

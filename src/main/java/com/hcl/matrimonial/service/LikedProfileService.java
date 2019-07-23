20e41f90d5fbe88286313a2f2f477f5328817bc5
Harsimar <simarkaur1295@gmail.com>
2019-07-23 15:26:35 +0530

    Mertimonial Search details

--- src/main/java/com/hcl/matrimonial/service/LikedProfileService.java
+++ src/main/java/com/hcl/matrimonial/service/LikedProfileService.java
@@ -2,8 +2,11 @@ package com.hcl.matrimonial.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.hcl.matrimonial.dto.LikeProfileDto;
 import com.hcl.matrimonial.entity.UserProfile;
+
 
 public interface LikedProfileService {
 	
--- src/main/java/com/hcl/matrimonial/service/LikedProfileServiceImpl.java
+++ src/main/java/com/hcl/matrimonial/service/LikedProfileServiceImpl.java
@@ -3,11 +3,13 @@ package com.hcl.matrimonial.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.hcl.matrimonial.dto.LikeProfileDto;
 import com.hcl.matrimonial.entity.UserProfile;
 import com.hcl.matrimonial.repository.LikedProfilesRepository;
 
+@Service
 public class LikedProfileServiceImpl implements LikedProfileService {
 
 	@Override

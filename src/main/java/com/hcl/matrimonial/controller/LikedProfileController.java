1e4c65726f7837416ba908dca61308a3756f9453
piyush <piyushr999@gmail.com>
2019-07-23 15:16:21 +0530

    done with like functionality

--- src/main/java/com/hcl/matrimonial/controller/LikedProfileController.java
+++ src/main/java/com/hcl/matrimonial/controller/LikedProfileController.java
@@ -1,12 +1,14 @@
 package com.hcl.matrimonial.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hcl.matrimonial.dto.LikeProfileDto;
+import com.hcl.matrimonial.entity.UserProfile;
 import com.hcl.matrimonial.service.LikedProfileService;
 
 @RestController
@@ -14,11 +16,9 @@ public class LikedProfileController {
 
 	@Autowired
 	private LikedProfileService likedProfileService;
-	
+
 	@PostMapping("/profile/like")
 	public ResponseEntity<?> likeProfile(@RequestBody LikeProfileDto profileDto) {
-		likedProfileService.likePrfiles(profileDto);
-		
-		return null;
+		return new ResponseEntity<UserProfile>(likedProfileService.likePrfiles(profileDto), HttpStatus.CREATED);
 	}
 }

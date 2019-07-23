6be80c4739c5e197ecba89e4e6f9942bf6306e3c
prajaktajoshi123 <joshiprajakta12493@gmail.com>
2019-07-23 16:37:35 +0530

    UserProfile test case

---
+++ src/test/java/com/hcl/matrimonial/matrimonial/service/UserProfileServiceTest.java
@@ -0,0 +1,11 @@
+package com.hcl.matrimonial.matrimonial.service;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserProfileServiceTest {
+
+	
+	
+}

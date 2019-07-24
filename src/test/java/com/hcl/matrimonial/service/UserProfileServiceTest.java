10b521409addce38a75b1fa6f1c4930382862633
prajaktajoshi123 <joshiprajakta12493@gmail.com>
2019-07-24 09:29:45 +0530

    test

--- src/test/java/com/hcl/matrimonial/service/UserProfileServiceTest.java
+++ src/test/java/com/hcl/matrimonial/service/UserProfileServiceTest.java
@@ -1,7 +1,10 @@
 package com.hcl.matrimonial.service;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+
+import java.sql.Date;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,6 +16,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import com.hcl.matrimonial.repository.UserProfileRepository;
 import com.hcl.matrimonial.dto.LoginDto;
+import com.hcl.matrimonial.dto.UserProfileDto;
 import com.hcl.matrimonial.entity.UserProfile;
 
 
@@ -27,7 +31,7 @@ public class UserProfileServiceTest {
 	@Mock
 	UserProfileRepository userprofilerepository;
 
-	
+	UserProfileDto userProfileDto;
 
 	@InjectMocks
 	UserProfileService userprofileservice;	
@@ -42,6 +46,25 @@ public class UserProfileServiceTest {
 		logindto.setPassword("password");
 		
 		
+		userProfileDto=new UserProfileDto();
+		
+		userProfileDto.setAddress("Pune");
+		//userprofile.setAge("34");
+		userProfileDto.setAnnualIncome(6700.0);
+		userProfileDto.setCity("Pune");
+		userProfileDto.setDob(new Date(1993-04-01).toLocalDate());
+		userProfileDto.setEducation("Msc");
+		userProfileDto.setEmailId("p@gmail.com");
+		userProfileDto.setFullName("Prajakta");
+		userProfileDto.setGender("Female");
+		userProfileDto.setHeight(5.7);
+		userProfileDto.setMobileNumber("897654");
+		userProfileDto.setNationality("Indian");
+		userProfileDto.setOccupation("JOb");
+		userProfileDto.setPassword("Praju");
+		userProfileDto.setUserName("Praju");
+		userProfileDto.setWeight(50.5);
+		
 	}
 	
 
@@ -50,5 +73,14 @@ public class UserProfileServiceTest {
 		Mockito.when(userprofilerepository.findByUserNameAndPassword("username", "password")).thenReturn(userprofile);
 		assertNotNull(userprofileservice.loginUser(logindto));
 	}
+	
+	/*
+	 * @Test public void testRegistration() {
+	 * Mockito.when(userprofilerepository.save(userprofile)).thenReturn(userprofile)
+	 * ; assert(userprofileservice.registerUser(userProfileDto));
+	 * 
+	 * }
+	 */
+	
 	
 }
